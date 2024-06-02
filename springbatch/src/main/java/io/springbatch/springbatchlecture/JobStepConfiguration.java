82f0be76a947e0cbb2d518fa9a3f1666c92d2835
whdqor94@naver.com <whdqor94@naver.com>
2024-06-02 19:13:11 +0900

    2024.06.02 JobStep 활용

--- springbatch/src/main/java/io/springbatch/springbatchlecture/JobStepConfiguration.java
+++ springbatch/src/main/java/io/springbatch/springbatchlecture/JobStepConfiguration.java
@@ -2,10 +2,14 @@ package io.springbatch.springbatchlecture;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.job.DefaultJobParametersExtractor;
+import org.springframework.batch.core.step.job.JobParametersExtractor;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -13,30 +17,51 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class TaskletStepArchitectureConfiguration {
+public class JobStepConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job batchJob() {
-        return this.jobBuilderFactory.get("batchJob")
-                .incrementer(new RunIdIncrementer())
-                .start(step1())
+    public Job parentJob() {
+        return this.jobBuilderFactory.get("parentJob")
+                .start(jobStep(null))
                 .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step jobStep(JobLauncher jobLauncher) {
+        return stepBuilderFactory.get("jobStep")
+                .job(childJob())
+                .launcher(jobLauncher)
+                .parametersExtractor(jobParametersExtractor())
                 .listener(new StepExecutionListener() {
-                    // Step 실행 전 작업 실행
                     @Override
                     public void beforeStep(StepExecution stepExecution) {
-
+                        stepExecution.getExecutionContext().putString("name", "user1");
                     }
 
-                    // Step 실행 후 작업 실행
                     @Override
                     public ExitStatus afterStep(StepExecution stepExecution) {
                         return null;
                     }
                 })
+                .build();
+    }
+
+    // SpringBatch에서 기본적으로 제공하는 클래스 -> DefaultJobParametersExtractor
+    // Bean 설정 필요x
+    private DefaultJobParametersExtractor jobParametersExtractor() {
+        DefaultJobParametersExtractor extractor = new DefaultJobParametersExtractor();
+        extractor.setKeys(new String[]{"name"});
+        return extractor;
+    }
+
+    @Bean
+    public Job childJob() {
+        return jobBuilderFactory.get("childJob")
+                .start(step1())
                 .build();
     }
 
@@ -46,7 +71,8 @@ public class TaskletStepArchitectureConfiguration {
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("stepContribution = " + stepContribution + ", chunkContext = " + chunkContext);
+                        // 예외 발생
+//                        throw new RuntimeException("step1 was failed");
                         return RepeatStatus.FINISHED;
                     }
                 })
@@ -59,12 +85,12 @@ public class TaskletStepArchitectureConfiguration {
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("stepContribution = " + stepContribution + ", chunkContext = " + chunkContext);
-                        return RepeatStatus.FINISHED;
+                        // 예외 발생
+                        throw new RuntimeException("step2 was failed");
+//                        return RepeatStatus.FINISHED;
                     }
                 })
                 .build();
     }
-
 
 }
