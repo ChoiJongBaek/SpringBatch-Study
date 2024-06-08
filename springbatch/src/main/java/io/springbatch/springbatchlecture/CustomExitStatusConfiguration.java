874cfd4df8f7cdcc8dc6d75eb3ba446605e649a4
whdqor94@naver.com <whdqor94@naver.com>
2024-06-08 19:25:47 +0900

    2024.06.08 ExitStatus 정의

--- springbatch/src/main/java/io/springbatch/springbatchlecture/CustomExitStatusConfiguration.java
+++ springbatch/src/main/java/io/springbatch/springbatchlecture/CustomExitStatusConfiguration.java
@@ -12,26 +12,19 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class TransitionConfiguration {
+public class CustomExitStatusConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job parentJob() {
+    public Job batchJob() {
         return this.jobBuilderFactory.get("batchJob")
                 .start(step1())
-                    .on("FAILED")
-                    .to(step2())
-                    .on("FAILED")
-                    .stop()
-                .from(step1())
-                    .on("*")
-                    .to(step3())
-                    .next(step4())
-                .from(step2())
-                    .on("*")
-                    .to(step5())
+                .on("FAILED")
+                .to(step2())
+                .on("PASS")
+                .stop()
                 .end()
                 .build();
     }
@@ -41,16 +34,7 @@ public class TransitionConfiguration {
         return stepBuilderFactory.get("step1")
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println(">> step1 has executed");
-                    contribution.setExitStatus(ExitStatus.FAILED);
-                    return RepeatStatus.FINISHED;
-                }).build();
-    }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println(">> step3 has executed");
+                    contribution.getStepExecution().setExitStatus(ExitStatus.FAILED);
                     return RepeatStatus.FINISHED;
                 }).build();
     }
@@ -61,24 +45,8 @@ public class TransitionConfiguration {
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println(">> step2 has executed");
                     return RepeatStatus.FINISHED;
-                }).build();
-    }
-
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println(">> step4 has executed");
-                    return RepeatStatus.FINISHED;
-                }).build();
-    }
-
-    @Bean
-    public Step step5() {
-        return stepBuilderFactory.get("step5")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println(">> step5 has executed");
-                    return RepeatStatus.FINISHED;
-                }).build();
+                })
+                .listener(new PassCheckingListener())
+                .build();
     }
 }
