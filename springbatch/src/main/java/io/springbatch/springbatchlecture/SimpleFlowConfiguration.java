9a2f1490fbf16f36ef4b3e53f6bd8e995f893fe4
whdqor94@naver.com <whdqor94@naver.com>
2024-07-05 15:06:50 +0900

    2024.07.05 SimpleFlow 아키텍처 흐름 파악

--- springbatch/src/main/java/io/springbatch/springbatchlecture/SimpleFlowConfiguration.java
+++ springbatch/src/main/java/io/springbatch/springbatchlecture/SimpleFlowConfiguration.java
@@ -23,45 +23,24 @@ public class SimpleFlowConfiguration {
     public Job job() {
         return this.jobBuilderFactory.get("batchJob")
                 .incrementer(new RunIdIncrementer())
-                .start(flow1())
-                    .on("COMPLETED")
-                    .to(flow2())
-                .from(flow1())
-                    .on("FAILED")
-                    .to(flow3())
+                .start(step1())
+                .on("COMPLETED").to(step2())
+                .from(step1())
+                .on("FAILED").to(flow())
                 .end()
                 .build();
     }
 
     @Bean
-    public Flow flow1() {
-        FlowBuilder<Flow> builder = new FlowBuilder<>("flow1");
-        builder.start(step1())
-                .next(step2())
+    public Flow flow() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+
+        flowBuilder.start(step2())
+                .on("*")
+                .to(step3())
                 .end();
 
-        return builder.build();
-    }
-
-    @Bean
-    public Flow flow2() {
-        FlowBuilder<Flow> builder = new FlowBuilder<>("flow2");
-        builder.start(flow3())
-                .next(step5())
-                .next(step6())
-                .end();
-
-        return builder.build();
-    }
-
-    @Bean
-    public Flow flow3() {
-        FlowBuilder<Flow> builder = new FlowBuilder<>("flow3");
-        builder.start(step3())
-                .next(step4())
-                .end();
-
-        return builder.build();
+        return flowBuilder.build();
     }
 
     @Bean
@@ -87,33 +66,6 @@ public class SimpleFlowConfiguration {
         return stepBuilderFactory.get("step3")
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println(">> step3 has executed");
-                    return RepeatStatus.FINISHED;
-                }).build();
-    }
-
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println(">> step4 has executed");
-                    return RepeatStatus.FINISHED;
-                }).build();
-    }
-
-    @Bean
-    public Step step5() {
-        return stepBuilderFactory.get("step5")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println(">> step5 has executed");
-                    return RepeatStatus.FINISHED;
-                }).build();
-    }
-
-    @Bean
-    public Step step6() {
-        return stepBuilderFactory.get("step6")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println(">> step6 has executed");
                     return RepeatStatus.FINISHED;
                 }).build();
     }
