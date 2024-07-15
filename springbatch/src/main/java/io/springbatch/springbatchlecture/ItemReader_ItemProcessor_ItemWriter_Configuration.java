c9db77ee347f81e5795833a2ead8e0a21e36f837
whdqor94@naver.com <whdqor94@naver.com>
2024-07-15 23:08:19 +0900

    2024.07.15 ItemReader ItemProcessor ItemWriter의 이해

--- springbatch/src/main/java/io/springbatch/springbatchlecture/ItemReader_ItemProcessor_ItemWriter_Configuration.java
+++ springbatch/src/main/java/io/springbatch/springbatchlecture/ItemReader_ItemProcessor_ItemWriter_Configuration.java
@@ -8,10 +8,10 @@ import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
-public class ChunkOrientedTaskletConfiguration {
+public class ItemReader_ItemProcessor_ItemWriter_Configuration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -38,21 +38,29 @@ public class ChunkOrientedTaskletConfiguration {
     @JobScope
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .<String, String>chunk(2)
-                .reader(new ListItemReader<>(Arrays.asList("item1","item2", "item3", "item4", "item5", "item6")))
-                .processor(new ItemProcessor<String, String>() {
-                    @Override
-                    public String process(String item) throws Exception {
-                        return "my_" + item;
-                    }
-                })
-                .writer(new ItemWriter<String>() {
-                    @Override
-                    public void write(List<? extends String> items) throws Exception {
-                        items.forEach(item -> System.out.println(item));
-                    }
-                })
+                .<Customer, Customer>chunk(3)
+                .reader(itemReader())
+                .processor(itemProcessor())
+                .writer(itemWirter())
                 .build();
+    }
+
+    @Bean
+    public ItemWriter<? super Customer> itemWirter() {
+        return new CustomItemWriter();
+    }
+
+    @Bean
+    public ItemProcessor<? super Customer,? extends Customer> itemProcessor() {
+        return new CustomItemProcessor();
+    }
+
+    @Bean
+    public ItemReader<Customer> itemReader() {
+        return new CustomItemReader(Arrays.asList(
+                new Customer("user1"),
+                new Customer("user2"),
+                new Customer("user3")));
     }
 
     @Bean
