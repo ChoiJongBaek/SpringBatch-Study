3aa500aa192b4e5c05d7d2abb87e3d2c73db66e4
whdqor94@naver.com <whdqor94@naver.com>
2024-07-27 16:14:05 +0900

    2024.07.27 FlatFileItemReader delimetedlinetokenize를 통한 변수 데이터 읽기 활용

--- springbatch/src/main/java/io/springbatch/springbatchlecture/FlatFilesConfiguration.java
+++ springbatch/src/main/java/io/springbatch/springbatchlecture/FlatFilesConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -49,28 +50,24 @@ public class FlatFilesConfiguration {
     }
 
     @Bean
-    public ItemReader itemReader() {
-
-        FlatFileItemReader<Customer> itemReader = new FlatFileItemReader<>();
-        itemReader.setResource(new ClassPathResource("/customer.csv"));
-
-        DefaultLineMapper<Customer> lineMapper = new DefaultLineMapper<>();
-        lineMapper.setLineTokenize(new DelimitedLineTokenizer());
-        lineMapper.setFieldSetMapper(new CustomerFieldSetMapper());
-
-        itemReader.setLineMapper(lineMapper);
-        itemReader.setLinesToSkip(1);
-
-        return itemReader;
-    }
-
-    @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println("step2 has executed");
                     return RepeatStatus.FINISHED;
                 })
+                .build();
+    }
+
+    @Bean
+    public ItemReader itemReader() {
+        return new FlatFileItemReaderBuilder<Customer>()
+                .name("flatFile")
+                .resource(new ClassPathResource("/customer.csv"))
+                .fieldSetMapper(new CustomerFieldSetMapper())
+                .linesToSkip(1)
+                .delimited().delimiter(",")
+                .names("name", "age", "year")
                 .build();
     }
 }
