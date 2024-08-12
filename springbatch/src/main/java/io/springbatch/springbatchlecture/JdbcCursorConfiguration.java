f3ddb85dca1613a159df145c65b59c7bb341222e
whdqor94@naver.com <whdqor94@naver.com>
2024-08-12 21:21:48 +0900

    2024.08.12 JbdbCursorItemReader 활용

--- springbatch/src/main/java/io/springbatch/springbatchlecture/JdbcCursorConfiguration.java
+++ springbatch/src/main/java/io/springbatch/springbatchlecture/JdbcCursorConfiguration.java
@@ -8,19 +8,24 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.json.JacksonJsonObjectReader;
-import org.springframework.batch.item.json.JsonItemReader;
 import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import javax.sql.DataSource;
+
 @RequiredArgsConstructor
 @Configuration
-public class JSONConfiguration {
+public class JdbcCursorConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private int chunkSize = 10;
+    private final DataSource dataSource;
 
     @Bean
     public Job job() {
@@ -33,21 +38,26 @@ public class JSONConfiguration {
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .<Customer, Customer>chunk(3)
-                .reader(customItemReader())
+                .<Customer, Customer>chunk(chunkSize)
+                .reader(customerItemReader())
                 .writer(customItemWriter())
                 .build();
     }
 
     @Bean
-    public ItemReader<? extends Customer> customItemReader() {
+    public ItemReader<Customer> customerItemReader() {
 
-        return new JsonItemReaderBuilder<Customer>()
-                .name("jsonReader")
-                .resource(new ClassPathResource("customer.json"))
-                .jsonObjectReader(new JacksonJsonObjectReader<>(Customer.class))
+        return new JdbcCursorItemReaderBuilder<Customer>()
+                .name("jdbcCursorItemReader")
+                //보통 fetchSize와 chunkSize를 동일하게 설정
+                .fetchSize(chunkSize)
+                .sql("select id, firstName, lastName, birthdate from customer where firstName like ? order by lastName, firstName")
+                .beanRowMapper(Customer.class)
+                .queryArguments("A%")
+                .dataSource(dataSource)
                 .build();
     }
+
 
     @Bean
     public ItemWriter<Customer> customItemWriter() {
