666f7d9bfbf83cf4fd05a969db3e8aacafbc856a
whdqor94@naver.com <whdqor94@naver.com>
2024-08-24 16:00:41 +0900

    2024.08.24 ItemReaderAdapter 활용

--- springbatch/src/main/java/io/springbatch/springbatchlecture/ItemReaderAdapterConfiguration.java
+++ springbatch/src/main/java/io/springbatch/springbatchlecture/ItemReaderAdapterConfiguration.java
@@ -8,27 +8,19 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.Order;
-import org.springframework.batch.item.database.PagingQueryProvider;
-import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.adapter.ItemReaderAdapter;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
-import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Configuration
-public class JpaPagingConfiguration {
+public class ItemReaderAdapterConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-    private final EntityManagerFactory entityManagerFactory;
 
     @Bean
     public Job job() throws Exception {
@@ -41,30 +33,33 @@ public class JpaPagingConfiguration {
     @Bean
     public Step step1() throws Exception {
         return stepBuilderFactory.get("step1")
-                .<Customer, Customer>chunk(10)
-                .reader(customerItemReader())
+                .<String, String>chunk(10)
+                .reader(customItemReader())
                 .writer(customItemWriter())
                 .build();
     }
 
     @Bean
-    public ItemReader<? extends Customer> customerItemReader() {
+    public ItemReader<String> customItemReader() {
 
-        return new JpaPagingItemReaderBuilder<Customer>()
-                .name("jpaPagingItemReader")
-                .entityManagerFactory(entityManagerFactory)
-                .pageSize(10)
-                .queryString("select c from Customer c")
-                .build();
+        ItemReaderAdapter<String> reader = new ItemReaderAdapter<>();
+        reader.setTargetObject(customService());
+        //Object에 있는 호출할 메소드 기재
+        reader.setTargetMethod("customRead");
+
+        return reader;
     }
 
+    @Bean
+    public Object customService() {
+
+        return new CustomService();
+    }
 
     @Bean
-    public ItemWriter<Customer> customItemWriter() {
+    public ItemWriter<String> customItemWriter() {
         return items -> {
-            for (Customer customer : items) {
-                System.out.println(customer.getAddress().getLocation());
-            }
+            System.out.println(items);
         };
     }
 }
