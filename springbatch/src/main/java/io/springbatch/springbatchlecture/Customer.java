422e883c4d314baf67f2054ba74da51bfcbae08f
whdqor94@naver.com <whdqor94@naver.com>
2024-09-18 00:59:32 +0900

    2024.09.18 동기 비동기의 비교

---
+++ springbatch/src/main/java/io/springbatch/springbatchlecture/Customer.java
@@ -0,0 +1,17 @@
+package io.springbatch.springbatchlecture;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+
+    private final long id;
+    private final String firstName;
+    private final String lastName;
+    private final Date birthdate;
+}
